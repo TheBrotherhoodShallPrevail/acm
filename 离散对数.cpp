38553808f4b2111129abd0ccb392bc8a7241b09b
hanxiaohao <835124694@qq.com>
2017-07-13 15:03:13 +0800

    Update
    原来的（好像）不能处理A和C互质的情况？

--- 离散对数.cpp
+++ 离散对数.cpp
@@ -6,59 +6,135 @@
 using namespace std;
 #define ll long long
 
-const int MOD = 76543;
-
-// 功能：BSGS算法求解离散对数：a^x = b (mod n),已知a,b,n求x
-// 接口：int BSGS(int a, int b, int n)
-// 输入：a,b,n
+// 功能：扩展BSGS算法求解离散对数：A ^ x = B (mod C), 已知A, B, C求x, C是不是质数都可以
+// 接口：int BabyStep(int A, int B, int C)
+// 输入：A, B, C
 // 输出：x
 
-int hs[MOD], head[MOD], next[MOD], id[MOD], top;
-
-void insert(int x, int y)
+struct Node
 {
-	int k = x % MOD;
-	hs[top] = x, id[top] = y, next[top] = head[k], head[k] = top++;
+	int idx;
+	int val;
+} baby[1000000];
+
+bool cmp(Node n1, Node n2)
+{
+	return n1.val != n2.val ? n1.val < n2.val : n1.idx < n2.idx;
 }
 
-int find(int x)
+int gcd(int a, int b)
 {
-	int k = x % MOD;
-	for (int i = head[k]; i != -1; i = next[i])
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+int extend_gcd(int a, int b, int &x, int &y)
+{
+	if (b == 0)
 	{
-		if (hs[i] == x)
-			return id[i];
+		x = 1;
+		y = 0;
+		return a;
+	}
+	int gcd = extend_gcd(b, a % b, x, y);
+	int t = x;
+	x = y;
+	y = t - a / b * y;
+	return gcd;
+}
+
+int inval(int a, int b, int n)
+{
+	int e, x, y;
+	extend_gcd(a, n, x, y);
+	e = ((ll)x * b) % n;
+	return e < 0 ? e + n : e;
+}
+
+int PowMod(int a, int b, int MOD)
+{
+	ll ret = 1 % MOD, t = a % MOD;
+	while (b)
+	{
+		if (b & 1)
+			ret = ((ll)ret * t) % MOD;
+		t = ((ll)t * t) % MOD;
+		b >>= 1;
+	}
+	return (int)ret;
+}
+
+int BinSearch(int num, int m)
+{
+	int low = 0, high = m - 1, mid;
+	while (low <= high)
+	{
+		mid = (low + high) >> 1;
+		if (baby[mid].val == num)	return baby[mid].idx;
+		if (baby[mid].val < num)	low = mid + 1;
+		else	high = mid - 1;
 	}
 	return -1;
 }
 
-// a^x = b (mod n)
-int BSGS(int a, int b, int n)
+//A ^ x = B (mod C)
+int BabyStep(int A, int B, int C)
 {
-	memset(head, -1, sizeof(head));
-	top = 1;
-	if (b == 1)	return 0;
-	int m = (int)sqrt(n * 1.0), j;
-	ll x = 1, p = 1;
-	for (int i = 0; i < m; i++, p = p * a % n)	insert(p * b % n, i);
-	for (ll i = m;; i += m)
+	ll tmp, D = 1 % C;
+	int temp;
+	for (int i = 0, tmp = 1 % C; i < 100; i++, tmp = ((ll)tmp * A) % C)
 	{
-		if ((j = find(x = x * p % n)) != -1)	return i - j;
-		if (i > n)	break;
+		if (tmp == B)
+			return i;
+	}
+	int d = 0;
+	while ((temp = gcd(A, C)) != 1)
+	{
+		if (B % temp) return -1;
+		d++;
+		C /= temp;
+		B /= temp;
+		D = ((A / temp) * D) % C;
+	}
+	int m = (int)ceil(sqrt((double)C));
+	for (int i = 0, tmp = 1 % C; i <= m; i++, tmp = ((ll)tmp * A) % C)
+	{
+		baby[i].idx = i;
+		baby[i].val = tmp;
+	}
+	sort(baby, baby + m + 1, cmp);
+	int cnt = 1;
+	for (int i = 1; i <= m; i++)
+	{
+		if (baby[i].val != baby[cnt - 1].val)
+			baby[cnt++] = baby[i];
+	}
+	int am = PowMod(A, m, C);
+	for (int i = 0; i <= m; i++, D = ((ll)(D * am)) % C)
+	{
+		int tmp = inval(D, B, C);
+		if (tmp >= 0)
+		{
+			int pos = BinSearch(tmp, cnt);
+			if (pos != -1)
+				return i * m + pos + d;
+		}
 	}
 	return -1;
 }
 
-//POJ 3243
+//POJ 2417
 int main()
 {
-	int x, z, k;
-	while (scanf("%d %d %d", &x, &z, &k) != EOF)
+	int P, B, N;
+	while (scanf("%d %d %d", &P, &B, &N) != EOF)
 	{
-		if (!x && !z && !k)	break;
-		// x^y = k (mod z)
-		int ans = BSGS(x, k, z);
-		if (ans == -1)	printf("No Solution\n");
+		// if (!P && !B && !N)	break;
+		// B ^ ans = N (mod P)
+		B %= P;
+		N %= P;
+		int ans = BabyStep(B, N, P);
+		if (ans < 0)	printf("no solution\n");
 		else	printf("%d\n", ans);
 	}
+	return 0;
 }
